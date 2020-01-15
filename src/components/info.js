97ebb4bfe8a810be927a583507c24144d4176437
Curt Poff <curt@poffdev.com>
2020-01-14 21:54:13 -0700

    new telegram link

--- src/components/info.js
+++ src/components/info.js
@@ -7,6 +7,7 @@ import {
   FaInstagram as Insta,
   FaEnvelope as Mail,
   FaTelegram as Telegram,
+  FaKeybase as Keybase,
 } from "react-icons/fa"
 
 export default function info() {
@@ -19,7 +20,7 @@ export default function info() {
       </div>
       <div className="h1 code mt-4 mb-3">async {"{"}</div>
       <div className="text-muted mx-5 my-4 h3 text-justify info">
-      I’ve worked as a web developer/producer/PM for the past 20 years at The Arizona Republic, Macworld, Walmart.com, and Nike. I’m married to the lovely <a href="https://gofitgirl.com" target="_blank">Go Fit Girl!</a> We recently relocated to Tucson after many years in the Bay Area and Portland. It's nice and sunny here, so I'm happy.
+      I’ve worked as a web developer/producer/PM for the past 20 years at The Arizona Republic, Macworld, Walmart.com, and Nike. I’m married to the lovely <a href="https://gofitgirl.com" target="_blank">Go Fit Girl!</a> Not long ago we relocated to Tucson after many years in the Bay Area and Portland. It's nice and sunny here, so I'm happy.
       </div>
       <div className="h1 code mt-2 mb-3">{"}"}</div>
       <div className="h1 mt-5">
@@ -31,6 +32,9 @@ export default function info() {
         </a>
         <a className="mr-5 icon" href="https://github.com/cpoff">
           <Github />
+        </a>
+        <a className="mr-5 icon" href="https://keybase.io/cpoff">
+          <Keybase />
         </a>
         <a className="mr-5 icon" href="https://t.me/cpoff">
           <Telegram />
